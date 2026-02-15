faa59fdd4b3fe78706a8e2720862bd1cdbd33b17
ankr19 <ankm335@gmail.com>
2026-02-15 13:33:59 +0530

    fixes: goolge keys

--- firebase/firebaseconfig.jsx
+++ firebase/firebaseconfig.jsx
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDfAD0xgRqmwH0Yq8ZNN_97H9AkgeQiowo",
-  authDomain: "webhosting-6076c.firebaseapp.com",
-  projectId: "webhosting-6076c",
-  storageBucket: "webhosting-6076c.firebasestorage.app",
-  messagingSenderId: "205011374368",
-  appId: "1:205011374368:web:ca91a80317969ffd92c745",
-  measurementId: "G-QJ8BJX0RMJ"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: 
 };
 
 // Initialize Firebase
